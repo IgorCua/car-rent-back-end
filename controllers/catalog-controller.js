29bd2bc0b83a6be5b9c283a162271374ca2d6179
IhorC. <igorchua@gmail.com>
2023-09-28 09:28:11 -0500

    working with filtered list

--- controllers/catalog-controller.js
+++ controllers/catalog-controller.js
@@ -3,17 +3,20 @@ const ctrlWrapper = require("../utils/ctrlWrapper");
 const Catalog = require("../models/catalog");
 
 const getCarslist = async (req, res) => {
-    // const { page = 1, limit = 8 } = req.query;
-    // const skip = (page - 1) * limit;
+    const { page = 1, limit = 8 } = req.query;
+    const skip = (page - 1) * limit;
     console.log(req.query);
-    const catalogList = await Catalog.find();
+    const catalogList = await Catalog.find('', '', {skip, limit});
     res.json(catalogList);
     // console.log(catalogList);
 }
 
 const getFilteredList = async (req, res) => {
-    const catalogList = await Catalog.find();
+    const { page = 1, limit = 8 } = req.query;
+    const skip = (page - 1) * limit;
+    const catalogList = await Catalog.find({}, '', {skip, limit});
     console.log(req.query);
+    res.json(catalogList);
 }
 
 module.exports = {
