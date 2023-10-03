d239674df962735dee8d5a70ce92e0fb51860087
IhorC. <igorchua@gmail.com>
2023-10-03 11:22:18 -0500

    handleFilter logs removed

--- helpers/handleFilter.js
+++ helpers/handleFilter.js
@@ -1,13 +1,12 @@
 const filterHandler = (obj) => {
     let resultObj = {};
+    
     for (const key in obj) {
         if (key !== "mileageFrom" && key !== "mileageTo") {
-            console.log("other", key, obj[`${key}`]);
             if(obj[`${key}`].length !== 0) resultObj[`${key}`] = obj[`${key}`];
         };
         
         if (key === "mileageFrom" && obj[`${key}`].length > 0){
-            console.log('KEY', key)
             if(resultObj.$and) resultObj.$and.push({mileage: {$gte: +obj[`${key}`]}});
             
             if(!resultObj.$and){
@@ -15,8 +14,8 @@ const filterHandler = (obj) => {
                 resultObj.$and.push({mileage: {$gte: +obj[`${key}`]}});
             }
         }; 
+        
         if (key === "mileageTo" && obj[`${key}`].length > 0){
-            console.log('KEY', key)
             if(resultObj.$and) resultObj.$and.push({mileage: {$lte: +obj[`${key}`]}});
             
             if(!resultObj.$and){
@@ -26,7 +25,7 @@ const filterHandler = (obj) => {
         };
     }
     
-    console.log("resultObj", resultObj)
+    // console.log("resultObj", resultObj);
     return resultObj;
 };
 
