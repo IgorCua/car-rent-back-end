3edcc3aa9c2d7786e529cdaa509efef4cad98ff0
IhorC. <igorchua@gmail.com>
2023-10-03 11:18:58 -0500

    filter helper works

--- helpers/handleFilter.js
+++ helpers/handleFilter.js
@@ -1,38 +1,32 @@
 const filterHandler = (obj) => {
-    // const reqObj = JSON.parse(obj);
-    let resultObj = {
-        
-    };
-    // console.log(obj);
+    let resultObj = {};
     for (const key in obj) {
-        // console.log('length', obj[`${key}`].length);
-        // resultObj.$and = obj[`${key}`];
-        // console.log('KEY', key)
         if (key !== "mileageFrom" && key !== "mileageTo") {
-            console.log(key, obj[`${key}`]);
+            console.log("other", key, obj[`${key}`]);
             if(obj[`${key}`].length !== 0) resultObj[`${key}`] = obj[`${key}`];
-            // resultObj[`${key}`] = obj[`${key}`];
         };
+        
         if (key === "mileageFrom" && obj[`${key}`].length > 0){
+            console.log('KEY', key)
+            if(resultObj.$and) resultObj.$and.push({mileage: {$gte: +obj[`${key}`]}});
+            
             if(!resultObj.$and){
-                console.log("$and", resultObj)
-            } 
+                resultObj.$and = [];
+                resultObj.$and.push({mileage: {$gte: +obj[`${key}`]}});
+            }
         }; 
-        // resultObj.$and.push({mileage: obj[`${key}`]});
-        if (key === "mileageto" && obj[`${key}`].length > 0){
+        if (key === "mileageTo" && obj[`${key}`].length > 0){
+            console.log('KEY', key)
+            if(resultObj.$and) resultObj.$and.push({mileage: {$lte: +obj[`${key}`]}});
+            
             if(!resultObj.$and){
-                console.log("$and", resultObj)
+                resultObj.$and = [];
+                resultObj.$and.push({mileage: {$lte: +obj[`${key}`]}});
             }
         };
-        // resultObj.$and.push({mileage: obj[`${key}`]});
-        
-        // if (key === "mileageFrom") resultObj[`mileage`].$gte = +obj[`${key}`];
-        // if (key === "mileageTo") resultObj[`mileage`].$lte = +obj[`${key}`];
-        // if(key === 'mileageFrom') resultObj.mileage.$and.push({})
-    } // console.log("OBJECT", key, obj[`${key}`]);
-    // console.log("reqObj", JSON.parse(obj));
-    // console.log("resultObj", resultObj);
-    // console.log("resultObj MAKE", resultObj.make);
+    }
+    
+    console.log("resultObj", resultObj)
     return resultObj;
 };
 
